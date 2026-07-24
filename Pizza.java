dffa1404765cb3005bfe3311d0f70fb2e4666e72
Atul Jadhav <jadhavatul3526@gmail.com>
2026-07-25 00:57:08 +0530

    java Oops Encapsulation example

---
+++ Pizza.java
@@ -0,0 +1,49 @@
+public class Pizza {
+    private String name = "Margherita";
+    private String specification = "Classic cheese and signature tomato herb sauce.";
+    private int price = 199;
+
+    //get methods
+    public String getName() {
+        return name;
+    }
+    public String getSpecification () {
+        return specification;
+    }
+    public int getPrice() {
+        return price;
+    }
+
+    // set methods
+    public void setName (String name){
+        this.name = name;
+    }
+    public void setSpecification( String specification){
+        this.specification = specification;
+    }
+    public void setPrice(int price){
+        this.price = price;
+    }
+}
+
+
+class ChangePizza {
+    public static void main(String[] args) {
+        Pizza p = new Pizza();
+
+        System.out.println("Before ");
+        System.out.println(p.getName());
+        System.out.println(p.getSpecification());
+        System.out.println(p.getPrice());
+        System.out.println(" ");
+
+        p.setName("Peppy Paneer");
+        p.setSpecification("Juicy paneer, crisp capsicum, and red paprika.");
+        p.setPrice(229);
+
+        System.out.println("After ");
+        System.out.println(p.getName());
+        System.out.println(p.getSpecification());
+        System.out.println(p.getPrice());
+    }
+}
