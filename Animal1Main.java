0bc141a8444dd6d518cefb5726904ec1047744d3
Atul Jadhav <jadhavatul3526@gmail.com>
2026-08-08 23:17:37 +0530

    Polymorphism Exam

---
+++ Animal1Main.java
@@ -0,0 +1,15 @@
+class Animal {
+     void sound() { 
+         System.out.println("Animal Sound"); 
+         } 
+         
+         } class Dog extends Animal { 
+             @Override void sound() { 
+                 System.out.println("Dog Barks"); 
+                 } 
+                 }
+                  public class Animal1Main { 
+                      public static void main(String[] args) { 
+                          Animal a = new Dog(); a.sound(); 
+                          } 
+                          }
