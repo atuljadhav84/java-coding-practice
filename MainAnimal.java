9e9049ff4c24f23ce714c46ffbdd1163692ede73
Atul Jadhav <jadhavatul3526@gmail.com>
2026-08-11 23:48:30 +0530

    Upcasting example

---
+++ MainAnimal.java
@@ -0,0 +1,25 @@
+
+class Animal
+{
+    void eat()
+    {
+        System.out.println("Animal is Eating");
+    }
+}
+
+class Dog extends Animal
+{
+    void bark()
+    {
+        System.out.println("Dog is Barking");
+    }
+}
+
+public class MainAnimal
+{
+    public static void main(String args[])
+    {
+        Animal a = new Dog();
+
+        a.eat();
+    }
