1c97a0520d916a32706f690586f2dc56b391318e
Atul Jadhav <jadhavatul3526@gmail.com>
2026-08-12 22:18:29 +0530

    Java Aggregation example

--- TrainPassenger.java
+++ TrainPassenger.java
@@ -1,9 +1,9 @@
 class Train {
-    String trainName ;
-    String location ;
-    int ticket = 500;
+    String trainName;
+    String location;
+    int ticket;
 
-    Train(String trainName, String location,int ticket){
+    Train(String trainName, String location, int ticket) {
         this.trainName = trainName;
         this.location = location;
         this.ticket = ticket;
@@ -12,36 +12,36 @@ class Train {
 
 class Passenger {
     String pName;
-    int age ;
-    String destination ;
+    int age;
+    String destination;
 
-    Train train;
-    Passenger(String pName, int age, String destination, Train train){
+    Train train;    // Only one Train object
+
+    Passenger(String pName, int age, String destination, Train train) {
         this.pName = pName;
         this.age = age;
         this.destination = destination;
-        
         this.train = train;
-
     }
 
-    void display(){
-        System.out.println("Passenger Name is: "+pName);
-        System.out.println("Passenger Age is: "+age);
-        System.out.println("Destination is: "+destination);
-        System.out.println();
-        System.out.println("Train name is: "+train.trainName);
-        System.out.println("Tarin location is: "+train.location);
-        System.out.println("Ticket price is: "+train.ticket);
+    void display() {
+        System.out.println("Passenger Name is: " + pName);
+        System.out.println("Passenger Age is: " + age);
+        System.out.println("Destination is: " + destination);
+
+        System.out.println("Train Name is: " + train.trainName);
+        System.out.println("Train Location is: " + train.location);
+        System.out.println("Ticket Price is: " + train.ticket);
     }
 }
 
 public class TrainPassenger {
     public static void main(String[] args) {
-        
-       Train t = new Train("Nanded Express", "Parbhani", 300);
-       Passenger p = new Passenger("Om Deshamush", 22, "Pune", t);
 
-       p.display();
+        Train t = new Train("Nanded Express", "Parbhani", 300);
+
+        Passenger p = new Passenger("Om Deshamush", 22, "Pune", t);
+
+        p.display();
     }
 }
