1d181cd0b7a140e629717fc4c437fafdedf81e92
Atul Jadhav <jadhavatul3526@gmail.com>
2026-08-17 22:35:59 +0530

    Abstraction example

---
+++ AbstractTv.java
@@ -0,0 +1,32 @@
+abstract class TV{
+    
+    abstract void turnOn();
+    abstract void turnOff();
+}
+
+// Concrete class implementing the abstract methods
+class TVRemote extends TV{
+    
+    @Override
+    void turnOn(){
+        
+        System.out.println("TV is turned ON.");
+    }
+
+    @Override
+    void turnOff(){
+        
+        System.out.println("TV is turned OFF.");
+    }
+}
+
+
+public class AbstractTv {
+    
+    public static void main(String[] args){
+        
+        TV remote = new TVRemote();
+        remote.turnOn();   
+        remote.turnOff();  
+    }
+}
